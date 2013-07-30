feb598387ca44088a3000ec657eea7cad0cd35ad
juanber <juanber84@gmail.com>
2013-07-30 22:45:28 +0200

    avances

--- static/js/main.js
+++ static/js/main.js
@@ -74,25 +74,85 @@ $(document).ready(function() {
 		if($("#pushblock3").is(':checked')) {  
 			setInterval(varName3, 100);
         }	
+
+        $('#avances').val('1')
+        $('#pushblock1').prop('checked', true)
+		$('#pushblock2').prop('checked', true)
+		$('#pushblock3').prop('checked', true)
+		$('#push1').removeClass('button-action')
+		$('#push2').removeClass('button-action')
+		$('#push3').removeClass('button-action')
+		$('#blocked').addClass('button-highlight')
+		$('#blocked').removeClass('button-caution')
+
 	});
 
 
+	$("#blocked").click(function() {
+		if ($('#avances').val() == '0') {
+			$('#blocked').addClass('button-highlight')
+			$('#blocked').removeClass('button-caution')
+			$('#avances').val('1')
+			$('#push1').removeClass('button-action')
+			$('#push2').removeClass('button-action')
+			$('#push3').removeClass('button-action')	
+	        $('#pushblock1').prop('checked', true)
+			$('#pushblock2').prop('checked', true)
+			$('#pushblock3').prop('checked', true)
+		}else{
+			$('#blocked').addClass('button-caution')
+			$('#blocked').removeClass('button-highlight')
+			$('#avances').val('0')					
+		}
+	});
+
 	$("#push1").click(function() {
-	  	var uno1  = Math.floor((Math.random()*10)+1);
-	  	$('.slot1').prepend('<img height=120 src="static/images/casino/'+uno1+'.png">')
-	  	$(".slot1 img:last-child").remove()
+		if ($('#avances').val() == 1) {
+		  	var uno1  = Math.floor((Math.random()*10)+1);
+		  	$('.slot1').prepend('<img height=120 src="static/images/casino/'+uno1+'.png">')
+		  	$(".slot1 img:last-child").remove()
+		}else{
+			if ($('#pushblock1').is(':checked')) {
+				$('#pushblock1').prop('checked', false)
+				$('#push1').addClass('button-action')
+			}else{
+				$('#pushblock1').prop('checked', true)
+				$('#push1').removeClass('button-action')
+			}
+		}
 	});
 
 	$("#push2").click(function() {
-	  	var uno1  = Math.floor((Math.random()*10)+1);
-	  	$('.slot2').prepend('<img height=120 src="static/images/casino/'+uno1+'.png">')
-	  	$(".slot2 img:last-child").remove()			
+		if ($('#avances').val() == 1) {
+	  		var uno1  = Math.floor((Math.random()*10)+1);
+		  	$('.slot2').prepend('<img height=120 src="static/images/casino/'+uno1+'.png">')
+		  	$(".slot2 img:last-child").remove()	
+		}else{
+			if ($('#pushblock2').is(':checked')) {
+				$('#pushblock2').prop('checked', false)
+				$('#push2').addClass('button-action')		
+			}else{
+				$('#pushblock2').prop('checked', true)
+				$('#push2').removeClass('button-action')		
+			}
+		}			
 	});
 
 	$("#push3").click(function() {
-	  	var uno1  = Math.floor((Math.random()*10)+1);
-	  	$('.slot3').prepend('<img height=120 src="static/images/casino/'+uno1+'.png">')
-	  	$(".slot3 img:last-child").remove()				  	
+		if ($('#avances').val() == 1) {
+		  	var uno1  = Math.floor((Math.random()*10)+1);
+		  	$('.slot3').prepend('<img height=120 src="static/images/casino/'+uno1+'.png">')
+		  	$(".slot3 img:last-child").remove()	
+		}else{
+			if ($('#pushblock3').is(':checked')) {
+				$('#pushblock3').prop('checked', false)
+				$('#push3').addClass('button-action')			
+			}else{
+				$('#pushblock3').prop('checked', true)
+				$('#push3').removeClass('button-action')			
+			}
+		}			
+			  	
 	});
 
 });
